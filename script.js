81962b64c81acb35b0c5319b8a93ada160a09514
mbisch11 <mbisch11@gmail.com>
2025-09-25 21:57:29 -0400

    Added deck API implementation

---
+++ script.js
@@ -0,0 +1,42 @@
+var userCards= [];
+var oppCards = [];
+var deckID;
+
+window.onload = function(){
+    var req = new XMLHttpRequest();
+    req.open('GET', 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1', true);
+    
+    req.onload = function(){
+        data = JSON.parse(this.response)
+        if (req.status == 200){
+            deckID = data.deck_id;
+        } else {
+            console.error(`There has been an error: Status ${req.status}`);
+        }
+    }
+    req.send();
+}
+
+function fetchCards(){
+    var cardReq = new XMLHttpRequest();
+    var giveUserCard = true;
+    cardReq.open('GET',`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=52`);
+
+    cardReq.onload = function(){
+        data = JSON.parse(this.response)
+        if (cardReq.status == 200){
+            data.cards.forEach(card => {
+                if(giveUserCard){
+                    userCards.push(card);
+                    giveUserCard = false;
+                } else {
+                    oppCards.push(card);
+                    giveUserCard = true;
+                }
+            });
+        } else {
+            console.error(`There has been an error: Status ${cardReq.status}`);
+        }
+    }
+    cardReq.send();
+}
